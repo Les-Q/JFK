a3d668898a396c6bbc1aec80e8077f0f6993cb7f
Bonny <schadenfreudeXYZ@gmail.com>
2017-10-29 21:33:28 +1000

    get rid of suppressMessages, warning is issued by libtesseract (i.e., C) anyway

--- R/JFK_functions.R
+++ R/JFK_functions.R
@@ -9,7 +9,7 @@ require(magick)
 
 import_ocr_doc <- function(full_pdf_url, page ){
   
-  bitmap <- pdftools::pdf_render_page(full_pdf_url, page=page, dpi = 400, numeric=TRUE)
+  bitmap <- pdftools::pdf_render_page(full_pdf_url, page=page, dpi = 400, numeric=FALSE)
   
   # bread-and-butter way
   # tiff::writeTIFF(bitmap, "C:/Users/Bonny/Documents/JFK/tiff/page.tiff")
@@ -18,12 +18,12 @@ import_ocr_doc <- function(full_pdf_url, page ){
   
   # cleaning the bitmap with magick gives typically better results; 
   # magick::image_ocr is based on tesseract::ocr
-  ocr_txt <- suppressWarnings( magick::image_read(bitmap) %>% #"C:/Users/Bonny/Documents/JFK/tiff/page.tiff") %>%
+  ocr_txt <- magick::image_read(bitmap,density=400) %>% #"C:/Users/Bonny/Documents/JFK/tiff/page.tiff") %>%
     magick::image_resize("2000") %>%
     magick::image_convert(colorspace = 'gray') %>% #assume all docs are black&white
-    magick::image_trim() %>%
-    magick::image_ocr() )
-  
+    magick::image_trim() %>% 
+    magick::image_ocr() 
+
   return(ocr_txt)
 }
 
