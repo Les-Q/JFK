ea70cf44fc6b781d53dc11ce53d1b8f679ddbcfc
Bonny <schadenfreudeXYZ@gmail.com>
2017-10-29 17:19:47 +1000

    suppressWarnings due to lack of resolution (dpi) info in input to image_ocr

--- R/JFK_functions.R
+++ R/JFK_functions.R
@@ -9,7 +9,7 @@ require(magick)
 
 import_ocr_doc <- function(full_pdf_url, page ){
   
-  bitmap <- pdftools::pdf_render_page(local_pdf, page=page, dpi = 400, numeric=TRUE)
+  bitmap <- pdftools::pdf_render_page(full_pdf_url, page=page, dpi = 400, numeric=TRUE)
   
   # bread-and-butter way
   # tiff::writeTIFF(bitmap, "C:/Users/Bonny/Documents/JFK/tiff/page.tiff")
@@ -18,11 +18,11 @@ import_ocr_doc <- function(full_pdf_url, page ){
   
   # cleaning the bitmap with magick gives typically better results; 
   # magick::image_ocr is based on tesseract::ocr
-  ocr_txt <- magick::image_read(bitmap) %>% #"C:/Users/Bonny/Documents/JFK/tiff/page.tiff") %>%
+  ocr_txt <- suppressWarnings( magick::image_read(bitmap) %>% #"C:/Users/Bonny/Documents/JFK/tiff/page.tiff") %>%
     magick::image_resize("2000") %>%
-    magick::image_convert(colorspace = 'gray') %>%
+    magick::image_convert(colorspace = 'gray') %>% #assume all docs are black&white
     magick::image_trim() %>%
-    magick::image_ocr()
+    magick::image_ocr() )
   
   return(ocr_txt)
 }
